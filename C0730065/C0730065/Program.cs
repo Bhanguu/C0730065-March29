db5f2aa9af1154a9134b56679171a36af95dd0d5
visualstudio <visualstudio@122-PC11>
2019-03-29 20:00:37 -0400

    8.00Pm

--- C0730065/C0730065/Program.cs
+++ C0730065/C0730065/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace C0730065
@@ -22,6 +23,11 @@ namespace C0730065
                 Console.WriteLine("Hello World");
           }
 
+        }
+        static void Download()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("Download complete");
         }
 
     }
